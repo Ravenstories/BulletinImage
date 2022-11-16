ee8ec629dc2ad6257d4a1d7f943e09f439e8d0ed
Jacob Nyyssønen Henriksen <jaco9515@zbc.dk>
2022-11-16 13:01:41 +0100

    init

--- app/src/main/java/com/example/bulletinimage/MainActivity.java
+++ app/src/main/java/com/example/bulletinimage/MainActivity.java
@@ -2,16 +2,53 @@ package com.example.bulletinimage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CaptureRequest;
+import android.media.ImageReader;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
+import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
- 
+    private static final String TAG = "AndroidCameraApi";
+    private Button btn_camera;
+    private Button btn_gallery;
+    private TextureView textureView;
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
 
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+
+    private String cameraId;
+    protected CameraDevice cameraDevice;
+    protected CameraCaptureSession cameraCaptureSession;
+    protected CaptureRequest.Builder captureRequestBuilder;
+    private Size imageDimensions;
+    private ImageReader imageReader;
+    private File file;
+    private File folder;
+    private String folderName = "MyPhotoDir";
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+    private Handler mBackroundHandler;
+    private HandlerThread mBackroundThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textureView = findViewById(R.id.iv_image);
     }
 }
